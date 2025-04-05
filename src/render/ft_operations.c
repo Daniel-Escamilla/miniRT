e97eaa424103b796fd54b520e02f250f7e4c686d
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-05 17:14:17 +0200

    Add vector operations: addition, subtraction, multiplication, and safe division

---
+++ src/render/ft_operations.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_operations.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 12:15:02 by descamil          #+#    #+#             */
+/*   Updated: 2025/04/05 12:23:55 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/render.h"
+
+t_vec3	ft_add(t_vec3 a, t_vec3 b)
+{
+	t_vec3	result;
+
+	result.x = a.x + b.x;
+	result.y = a.y + b.y;
+	result.z = a.z + b.z;
+	return (result);
+}
+
+t_vec3	ft_subtract(t_vec3 a, t_vec3 b)
+{
+	t_vec3	result;
+
+	result.x = a.x - b.x;
+	result.y = a.y - b.y;
+	result.z = a.z - b.z;
+	return (result);
+}
+
+t_vec3	ft_multiply(t_vec3 a, t_vec3 b)
+{
+	t_vec3	result;
+
+	result.x = a.x * b.x;
+	result.y = a.y * b.y;
+	result.z = a.z * b.z;
+	return (result);
+}
+
+static float	safe_divide(float a, float b)
+{
+	if (b == 0)
+		return 0;
+	return (a / b);
+}
+
+t_vec3	ft_divide(t_vec3 a, t_vec3 b)
+{
+	t_vec3	result;
+
+	result.x = safe_divide(a.x, b.x);
+	result.y = safe_divide(a.y, b.y);
+	result.z = safe_divide(a.z, b.z);
+	return (result);
+}
