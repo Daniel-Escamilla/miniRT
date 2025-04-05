46ee086ca7e53b9a140ec670db63273768e0d500
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-05 17:14:26 +0200

    Implement ray initialization in ft_per_pixel and refactor per-pixel rendering logic

--- src/render/ft_per_pixel.c
+++ src/render/ft_per_pixel.c
@@ -6,21 +6,38 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 15:29:21 by descamil          #+#    #+#             */
-/*   Updated: 2025/04/04 15:59:28 by descamil         ###   ########.fr       */
+/*   Updated: 2025/04/05 13:39:06 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/render.h"
 
-void	ft_init_values()
+t_ray ft_init_ray(t_image *image, t_vec2 coord)
 {
-	
+    t_ray ray;
+    t_camera *camera = image->objects->camera;
+
+    t_vec3 horizontal_scaled = ft_multiply(camera->horizontal, ft_float_to_vec3(coord.x));
+    t_vec3 vertical_scaled = ft_multiply(camera->vertical, ft_float_to_vec3(coord.y));
+    ray.origin = camera->position;
+    ray.direction = ft_add(horizontal_scaled, vertical_scaled);
+    ray.direction = ft_add(ray.direction, camera->l_left_corner);
+    ray.direction = ft_normalice(ft_subtract(ray.direction, ray.origin));
+    return (ray);
 }
+
 
 t_vec3	ft_per_pixel(t_image *image, t_vec2 coord)
 {
-	ft_init_values(image);
-	(void)image;
-	(void)coord;
+	t_image	*current;
+	t_ray	ray;
+
+	ray = ft_init_ray(image, coord);
+	current = image;
+	if (current->objects->sphere)
+	{
+		ft_render_sphere(current, coord);
+		current->objects->sphere = current->objects->sphere->next;
+	}
 	return ((t_vec3){{1.0f, 0.0f, 0.5f}});
 }
