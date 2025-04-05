d80b1ff5e5e7f77698caa93405ff2aa1c517221d
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-05 17:14:07 +0200

    Add vector operations: cross product and vector initialization functions

--- src/render/ft_maths.c
+++ src/render/ft_maths.c
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 15:53:45 by descamil          #+#    #+#             */
-/*   Updated: 2025/04/04 15:54:07 by descamil         ###   ########.fr       */
+/*   Updated: 2025/04/05 12:42:24 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,34 @@ t_vec3	ft_normalice(t_vec3 vec)
 	vec.y /= magnitud;
 	vec.z /= magnitud;
 	return (vec);
+}
+
+t_vec3	ft_cross(t_vec3 a, t_vec3 b)
+{
+	t_vec3	result;
+
+	result.x = a.y * b.z - a.z * b.y;
+	result.y = a.z * b.x - a.x * b.z;
+	result.z = a.x * b.y - a.y * b.x;
+	return (result);
+}
+
+t_vec3	ft_vec3(float a, float b, float c)
+{
+	t_vec3	result;
+
+	result.x = a;
+	result.y = b;
+	result.z = c;
+	return (result);
+}
+
+t_vec3	ft_float_to_vec3(float a)
+{
+	t_vec3	result;
+
+	result.x = a;
+	result.y = a;
+	result.z = a;
+	return (result);
 }
