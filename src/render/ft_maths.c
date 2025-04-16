2cf0fbea58d685fe052fd52cc7ff918e6e18ba72
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-04-16 12:53:32 +0200

    Refactor ft_maths.c: update author information and remove unused ft_normalice function

--- src/render/ft_maths.c
+++ src/render/ft_maths.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_maths.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 15:53:45 by descamil          #+#    #+#             */
-/*   Updated: 2025/04/09 19:33:56 by descamil         ###   ########.fr       */
+/*   Updated: 2025/04/16 12:11:14 by daniel-esca      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,10 @@
 
 double	ft_dot(t_vec3 a, t_vec3 b)
 {
-	double result;
+	double	result;
 
 	result = (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
-
 	return (result);
-}
-t_vec3	ft_normalice(t_vec3 vec)
-{
-	double	magnitud;
-
-	magnitud = ft_dot(vec, vec);
-	magnitud = sqrt(magnitud);
-
-	if (magnitud == 0.0f)
-		return (vec);
-	vec.x /= magnitud;
-	vec.y /= magnitud;
-	vec.z /= magnitud;
-	return (vec);
 }
 
 t_vec3	ft_cross(t_vec3 a, t_vec3 b)
@@ -45,12 +30,12 @@ t_vec3	ft_cross(t_vec3 a, t_vec3 b)
 	return (result);
 }
 
-t_vec3	ft_vec3(double a, double b, double c)
+double	ft_length_squared(t_vec3 vec)
 {
-	t_vec3	result;
+	return (vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+}
 
-	result.x = a;
-	result.y = b;
-	result.z = c;
-	return (result);
+t_vec3	ft_norm(t_vec3 v)
+{
+	return (ft_scale(v, 1 / sqrt(ft_length_squared(v))));
 }
