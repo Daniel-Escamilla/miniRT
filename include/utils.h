d4ebcfd536aa65e093b9ed39248dac8132b8ce42
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-04-16 12:52:21 +0200

    Update utils.h to correct author information and reorganize function declarations

--- include/utils.h
+++ include/utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/03 12:37:54 by descamil          #+#    #+#             */
-/*   Updated: 2025/02/14 21:37:45 by descamil         ###   ########.fr       */
+/*   Created: 2025/04/16 12:35:06 by daniel-esca       #+#    #+#             */
+/*   Updated: 2025/04/16 12:46:58 by daniel-esca      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,24 @@
 
 # include "../minirt.h"
 
+// ft_free_struct.c
+void	ft_safe_free(void **ptr);
+void	ft_free_list_general(void **list);
+void	ft_free_all(t_image *image);
+
 // ft_atof.c
+double	convert_integer_part(char *str, int *i, int *error);
+double	convert_decimal_part(char *str, int *i, int *error);
+int		ft_all_numbers(char *str);
 double	ft_atof(char *str);
 
 // ft_atoi_mod.c
+int		calculate_result(char *str, int *i, int *error, int sign);
 int		ft_atoi_mod(char *str, int *error);
 
-// ft_print_struct.c
-void	print_objects(t_objects *objects);
-void	print_vec3(t_vec3 vec);
-
-// ft_free_struct.c
-void	ft_free_all(t_image *image);
-void	ft_free_list_general(void **list);
-
 // ft_strstr_utils.c
+int		ft_strnstrstr(char **str, char *locate);
 int		ft_strstr_len(char **str);
 void	ft_strstr_free(char **str);
-char	**ft_strstr_dup(char **str);
-int		ft_strnstrstr(char **str, char *locate);
-char	**ft_strstr_join(char **str, char **str1);
 
 #endif
