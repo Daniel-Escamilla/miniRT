88e72ff92469516b2482ed663bed66b7d2e82909
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-04-16 12:51:56 +0200

    Update main.h to correct author information and reorganize function declarations

--- include/main.h
+++ include/main.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/02 18:02:54 by descamil          #+#    #+#             */
-/*   Updated: 2025/02/14 21:51:57 by descamil         ###   ########.fr       */
+/*   Created: 2025/04/16 12:36:30 by daniel-esca       #+#    #+#             */
+/*   Updated: 2025/04/16 12:36:31 by daniel-esca      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,19 @@
 
 # include "../minirt.h"
 
-// ft_initialize.c
-// void	ft_initialize(t_image *image);
+// ft_normalice_values.c
+void	ft_normalice_planes(t_plane *plane);
+void	ft_normalice_cylinders(t_cylinder *cylinder);
+t_vec3	ft_normalice_camera(t_vec3 v);
+void	ft_normalice_values(t_image *image);
 
 // ft_window.c
 int		ft_end_program(t_image *image);
 void	ft_create_window(t_mlx *data, t_image *image);
+
+// ft_main.c
+int		ft_hooks(int key, t_image *image);
+void	ft_initialize(t_image *image);
+int		main(int argc, char **argv);
 
 #endif
