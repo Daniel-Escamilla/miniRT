938811009cb5cc210015e212f5cbd9e31955513e
Daniel-Escamilla <Daniel.escamilla.bq@gmail.com>
2025-04-16 12:52:56 +0200

    Update author information in ft_window.c and improve formatting

--- src/main/ft_window.c
+++ src/main/ft_window.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_window.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daniel-escamilla <daniel-escamilla@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 18:12:08 by descamil          #+#    #+#             */
-/*   Updated: 2025/04/04 12:24:20 by descamil         ###   ########.fr       */
+/*   Updated: 2025/04/16 11:44:48 by daniel-esca      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,11 @@ int	ft_end_program(t_image *image)
 void	ft_create_window(t_mlx *data, t_image *image)
 {
 	image->mlx = mlx_init();
-	image->mlx_win = mlx_new_window(image->mlx, image->width, image->height, "MiniRT");
+	image->mlx_win = mlx_new_window(image->mlx,
+			image->width, image->height, "Minirt");
 	if (image->mlx_win == NULL)
 		ft_end_program(image);
 	data->img = mlx_new_image(image->mlx, image->width, image->height);
-	data->addr = mlx_get_data_addr(data->img, &data->bits_per_pixel, &data->line_length, &data->endian);
+	data->addr = mlx_get_data_addr(data->img,
+			&data->bits_per_pixel, &data->line_length, &data->endian);
 }
